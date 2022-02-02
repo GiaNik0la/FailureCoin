fea0f360c9afe6c386b3debf6d4052ea1867f8d1
GiaNik0la <58341157+GiaNik0la@users.noreply.github.com>
2022-02-02 10:08:54 -0800

    finish (test)

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
-import * as crypto from 'crypto';
-
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto = __importStar(require("crypto"));
 class Transaction {
-    constructor(
-        public amount: number,
-        public payer: string,
-        public reciever: string
-    ) { }
-
+    constructor(amount, payer, reciever) {
+        this.amount = amount;
+        this.payer = payer;
+        this.reciever = reciever;
+    }
     toString() {
         return JSON.stringify(this);
     }
 }
-
 class Block {
-    public nonce: number = Math.floor(Math.random() * 999999999);
-
-    constructor(
-        public prevHash: string,
-        public transaction: Transaction,
-        public ts = Date.now()
-    ) { }
-
+    constructor(prevHash, transaction, ts = Date.now()) {
+        this.prevHash = prevHash;
+        this.transaction = transaction;
+        this.ts = ts;
+        this.nonce = Math.floor(Math.random() * 999999999);
+    }
     get hash() {
         const str = JSON.stringify(this);
         const hash = crypto.createHash('SHA256');
@@ -28,85 +44,61 @@ class Block {
         return hash.digest('hex');
     }
 }
-
 class Chain {
-    public static instance = new Chain();
-
-    chain: Block[];
-
     constructor() {
         this.chain = [new Block('', new Transaction(100, "genesis", "satoshi"))];
     }
-
     get lastBlock() {
         return this.chain[this.chain.length - 1];
     }
-
-    mine(nonce: number) {
+    mine(nonce) {
         let solution = 1;
         console.log("mining failure coin...");
-
-        while(true) {
+        while (true) {
             const hash = crypto.createHash('MD5');
             hash.update((nonce + solution).toString()).end();
-
             const attempt = hash.digest('hex');
-
-            if(attempt.substr(0,4) === '0000') {
-                console.log(`Solved! Solution is: ${solution}`)
+            if (attempt.substr(0, 4) === '0000') {
+                console.log(`Solved! Solution is: ${solution}`);
                 return solution;
             }
-
             solution++;
         }
     }
-    
-    addBlock(transaction: Transaction, senderPublicKey: string, signature: Buffer) {
+    addBlock(transaction, senderPublicKey, signature) {
         const verifier = crypto.createVerify('SHA256');
         verifier.update(transaction.toString());
-
-        const isValid: boolean = verifier.verify(senderPublicKey, signature);
-
-        if(isValid) {
+        const isValid = verifier.verify(senderPublicKey, signature);
+        if (isValid) {
             const newBlock = new Block(this.lastBlock.hash, transaction);
             this.mine(newBlock.nonce);
             this.chain.push(newBlock);
         }
     }
 }
-
+Chain.instance = new Chain();
 class Wallet {
-    public publicKey: string;
-    public privateKey: string;
-
     constructor() {
         const keypair = crypto.generateKeyPairSync('rsa', {
             modulusLength: 2048,
             publicKeyEncoding: { type: 'spki', format: 'pem' },
             privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
         });
-
         this.privateKey = keypair.privateKey;
         this.publicKey = keypair.publicKey;
     }
-
-    sendMoney(amount: number, payeePublicKey: string) {
-        const transaction = new Transaction(amount, this.publicKey, payeePublicKey)
-
+    sendMoney(amount, payeePublicKey) {
+        const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
         const sign = crypto.createSign('SHA256');
         sign.update(transaction.toString()).end();
-
         const signature = sign.sign(this.privateKey);
         Chain.instance.addBlock(transaction, this.publicKey, signature);
     }
 }
-
 const satoshi = new Wallet();
 const Jemala = new Wallet();
 const Giorgi = new Wallet();
-
 satoshi.sendMoney(5000, Jemala.publicKey);
 Jemala.sendMoney(246, Giorgi.publicKey);
 Giorgi.sendMoney(52, Jemala.publicKey);
-
 console.log(Chain.instance);
